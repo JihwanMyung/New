70a9c4c6b21c75b6bc432a459b6089cb317497d2
Git <git@gitbox.test>
2015-01-14 14:56:14 +0900

    fixed bug

--- summator.py
+++ summator.py
@@ -1,14 +1,16 @@
 """
-[TODO: overview of this module]
+sum_n(n)
+--
+exercise function
 """
 
 
 def sum_n(n):
     """
-    [TODO documentation]
+    sums n in the range(n) and returns the result
     """
     total = 0
-    for i in range(n):
+    for i in range(1,n+1):
         total += i
     
     return total
