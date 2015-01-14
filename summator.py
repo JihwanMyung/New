272de5c8b62bb83e428890e5be9fe4c08337373b
Git <git@gitbox.test>
2015-01-14 14:41:12 +0900

    newly committed

---
+++ summator.py
@@ -0,0 +1,14 @@
+"""
+[TODO: overview of this module]
+"""
+
+
+def sum_n(n):
+    """
+    [TODO documentation]
+    """
+    total = 0
+    for i in range(n):
+        total += i
+    
+    return total
